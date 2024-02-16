5da3aea3e37ab0d9dcd4fd867a1e911841a907cf
Yulya1511 <glukhikh.julia@yandex.ru>
2024-02-16 21:13:49 +0300

    second commit

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,9 +2,11 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println(firstMethod("Привет, мой Шерлок"));
+        System.out.println("Привет всем");
     }
 
     public static String firstMethod (String s){
         return s;
+
     }
 }
